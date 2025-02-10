a84ec19c4d634dc3b1fa89a67278aef069bc55dc
Lucas Cabral <lucassimple@hotmail.com>
2025-02-10 16:09:19 -0300

    Version 1.0.3

--- src/app/api/api/route.tsx
+++ src/app/api/api/route.tsx
@@ -1,14 +1,29 @@
+// import { NextResponse } from "next/server";
+// import { cookies } from "next/headers";
+
+// export async function GET() {
+//     try {
+//         cookies().delete(`${process.env.TOKEN_CREADO}`)
+
+//         return NextResponse.json({ message: "Cookie eliminada", status: 200 })
+
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
 export async function GET() {
     try {
-        //const eliminarCookie = cookies().delete(`${process.env.TOKEN_CREADO}`)
-        cookies().delete(`${process.env.TOKEN_CREADO}`)
+        // Eliminar la cookie
+        cookies().delete(process.env.TOKEN_CREADO as string);
 
-        return NextResponse.json({ message: "Cookie eliminada", status: 200 })
-
+        // Retornar respuesta exitosa
+        return NextResponse.json({ message: "Cookie eliminada", status: 200 });
     } catch (error) {
-        console.log(error)
+        // Manejo de errores
+        console.error("Error al eliminar la cookie:", error);
+        return NextResponse.json({ message: "Error al eliminar la cookie", status: 500 });
     }
 }
