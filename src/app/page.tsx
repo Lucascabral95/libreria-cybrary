33f93c09c2cc04676f7528dd08b105399673a40e
Lucas Cabral <lucassimple@hotmail.com>
2025-02-11 16:42:48 -0300

    Version 1.4.5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ const Home: React.FC = () => {
     const obtenerProps = async () => {
       try {
         const response = await axios.get(`${process.env.NEXT_PUBLIC_PATH_EXTERNAL}/api/v1/product/with-author`, {
-          withCredentials: true,
+          // withCredentials: true,
           headers: {
             "Content-Type": "application/json",
           }
