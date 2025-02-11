1621d0083a6f2c1230113eab2ea6fe210d53e926
Lucas Cabral <lucassimple@hotmail.com>
2025-02-11 14:32:45 -0300

    Version 1.3.3

--- src/app/api/api/test/route.tsx
+++ src/app/api/api/test/route.tsx
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
     try {
         // const response = await axios.get(`https://nest-app-6t3h.onrender.com/api/v1/product`)
-        const response = await axios.get(`https://nest-app-6t3h.onrender.com/api/v1/product/with-product`)
+        const response = await axios.get(`https://nest-app-6t3h.onrender.com/api/v1/product/with-author`)
 
         if (response.status === 200) {
             return NextResponse.json(response.data)
